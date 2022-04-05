8bf572f1231e931f2f0219f2eb425d81560941fc
MaliciousJ <junwork123@gmail.com>
2022-04-06 01:09:57 +0900

    # fix 임시 비활성화

--- src/main/java/chatbot/client/controller/ChatBotController.java
+++ src/main/java/chatbot/client/controller/ChatBotController.java
@@ -30,36 +30,35 @@ import java.util.List;
 @Slf4j
 @EnableScheduling
 @RequiredArgsConstructor
-@RequestMapping("/")
 @Controller
 public class ChatBotController {
     private final ChatBot chatBot;
 
-    @RequestMapping
-    public String hello(){
-        return "hello";
-    }
-
-    @GetMapping("hello")
-    public void awaken(){
-        log.info("waked Up! : {}", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
-    }
-    @PostMapping("hello")
-    public void startSchedule() throws IOException {
-        noSleep();
-    }
-
-    @Scheduled(cron = "10 * * * * *")
-    public void noSleep() {
-        wakeUpSelf();
-        wakeUpSelfLocal();
-    }
-    public void wakeUpSelf(){
-        WebClient.create("https://chatbot-junwork.herokuapp.com/").get();
-    }
-    public void wakeUpSelfLocal(){
-        WebClient.create("https://localhost:8080/").get();
-    }
+//    @RequestMapping
+//    public String hello(){
+//        return "hello";
+//    }
+//
+//    @GetMapping("hello")
+//    public void awaken(){
+//        log.info("waked Up! : {}", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+//    }
+//    @PostMapping("hello")
+//    public void startSchedule() throws IOException {
+//        noSleep();
+//    }
+//
+//    @Scheduled(cron = "10 * * * * *")
+//    public void noSleep() {
+//        wakeUpSelf();
+//        wakeUpSelfLocal();
+//    }
+//    public void wakeUpSelf(){
+//        WebClient.create("https://chatbot-junwork.herokuapp.com/").get();
+//    }
+//    public void wakeUpSelfLocal(){
+//        WebClient.create("https://localhost:8080/").get();
+//    }
 
 
 }
