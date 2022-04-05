5453dbd3e647d6bf6b0a27c3cd5671f9cec6dc33
MaliciousJ <junwork123@gmail.com>
2022-04-06 00:59:02 +0900

    # chore webflux의존성 추가(webclient 사용) / # refact ChatBotController ChatBot 생성 코드 의존성 제거

--- src/main/java/chatbot/client/controller/ChatBotController.java
+++ src/main/java/chatbot/client/controller/ChatBotController.java
@@ -4,43 +4,62 @@ import chatbot.client.common.chatbot.ChatBot;
 import chatbot.client.common.chatbot.ChatBotFactory;
 import chatbot.client.common.command.Command;
 import chatbot.client.common.command.CommandBuilder;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
 
+import java.io.IOException;
+import java.net.*;
+import java.net.http.HttpRequest;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Slf4j
+@EnableScheduling
+@RequiredArgsConstructor
 @RequestMapping("/")
+@Controller
 public class ChatBotController {
     private final ChatBot chatBot;
 
-    public ChatBotController(ChatBotFactory chatBotFactory, CommandBuilder commandFactory) {
-        List<Command> commands = commandFactory.getCommands();
-        ChatBot createdChatBot = chatBotFactory.CreateChatBot(commands);
-        this.chatBot = createdChatBot;
+    @RequestMapping
+    public String hello(){
+        return "hello";
     }
 
-    private ChatBotController(Builder builder) {
-        List<Command> commands = builder.commandFactory.getCommands();
-        ChatBot createdChatBot = builder.chatBotFactory.CreateChatBot(commands);
-        this.chatBot = createdChatBot;
+    @GetMapping("hello")
+    public void awaken(){
+        log.info("waked Up! : {}", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
     }
-    public static class Builder{
-        private final ChatBotFactory chatBotFactory;
-        private final CommandBuilder commandFactory;
-        public Builder(ChatBotFactory chatBotFactory, CommandBuilder commandFactory) {
-            this.chatBotFactory = chatBotFactory;
-            this.commandFactory = commandFactory;
-        }
-
-        public ChatBotController build(){
-            return new ChatBotController(this);
-        }
+    @PostMapping("hello")
+    public void startSchedule() throws IOException {
+        noSleep();
     }
 
-//    @GetMapping
-//    public String hello() {
-//        return "index";
-//    }
+    @Scheduled(cron = "10 * * * * *")
+    public void noSleep() {
+        wakeUpSelf();
+        wakeUpSelfLocal();
+    }
+    public void wakeUpSelf(){
+        WebClient.create("https://chatbot-junwork.herokuapp.com/").get();
+    }
+    public void wakeUpSelfLocal(){
+        WebClient.create("https://localhost:8080/").get();
+    }
+
 
 }
