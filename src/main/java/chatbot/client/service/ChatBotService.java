ac69c603b172e83b2d09d87374b79c9d15a5b708
MaliciousJ <junwork123@gmail.com>
2022-08-24 17:44:43 +0900

    #revert 구조 정리를 위해 복원
    #revert 구조 정리를 위해 복원

--- src/main/java/chatbot/client/service/ChatBotService.java
+++ src/main/java/chatbot/client/service/ChatBotService.java
@@ -1,7 +1,9 @@
 package chatbot.client.service;
 
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
 
 public interface ChatBotService {
     public String doService(@Nullable String option, @NonNull String content);
---
+++ src/main/java/chatbot/client/service/PingPongService.java
@@ -0,0 +1,14 @@
+package chatbot.client.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PingPongService implements ChatBotService {
+    @Override
+    public String doService(String option, String content) {
+        log.info("option = {}, content = {}", option, content);
+        return "Pong!";
+    }
+}
