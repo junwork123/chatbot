6d3bd9168820a0440f6d7de3186c8cfaa02901db
Junhyeok Choi <junwork123@gmail.com>
2022-08-29 16:31:44 +0900

    git guardian 정책 준수를 위한 key 내역 삭제

--- src/main/java/chatbot/client/utils/ConstUtils.java
+++
@@ -1,7 +0,0 @@
-package chatbot.client.utils;
-
-public class ConstUtils {
-    private ConstUtils() {}
-    public static final String JSON_PATH = "src/main/resources/commands/";
-    public static final String JSON_PATTERN = ".json";
-}
