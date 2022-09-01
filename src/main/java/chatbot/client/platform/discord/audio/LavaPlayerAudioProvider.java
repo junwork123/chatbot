d2ccefc88fd372a5b78cb7842a80da020144d974
Junhyeok Choi <junwork123@gmail.com>
2022-09-01 11:37:06 +0900

    #feat : discord audio 관련 클래스 추가

--- src/main/java/chatbot/client/platform/discord/audio/LavaPlayerAudioProvider.java
+++ src/main/java/chatbot/client/platform/discord/audio/LavaPlayerAudioProvider.java
@@ -1,4 +1,4 @@
-package chatbot.client.platform.discord.audioProvider;
+package chatbot.client.platform.discord.audio;
 
 import com.sedmelluq.discord.lavaplayer.format.StandardAudioDataFormats;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
@@ -8,39 +8,41 @@ import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame;
 import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
 import discord4j.voice.AudioProvider;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
 
 import java.nio.ByteBuffer;
 
 
-
+@Getter
+@Component
 public final class LavaPlayerAudioProvider extends AudioProvider {
-    public static AudioProvider createAudioProvider(){
-        // Creates AudioPlayer instances and translates URLs to AudioTrack instances
-        AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
-
-        // This is an optimization strategy that Discord4J can utilize. It is not important to understand
-        playerManager.getConfiguration().setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
-
-        // Allow playerManager to parse remote sources like YouTube links
-        AudioSourceManagers.registerRemoteSources(playerManager);
-
-        // Create an AudioPlayer so Discord4J can receive audio data
-        AudioPlayer player = playerManager.createPlayer();
-
-        // We will be creating LavaPlayerAudioProvider in the next step
-        AudioProvider provider = new LavaPlayerAudioProvider(player);
-
-        return provider;
-    }
+    private final AudioPlayerManager playerManager;
     private final AudioPlayer player;
+
+    private final TrackScheduler scheduler;
     private final MutableAudioFrame frame = new MutableAudioFrame();
 
-    public LavaPlayerAudioProvider(final AudioPlayer player) {
+    public LavaPlayerAudioProvider() {
         // Allocate a ByteBuffer for Discord4J's AudioProvider to hold audio data for Discord
         super(ByteBuffer.allocate(StandardAudioDataFormats.DISCORD_OPUS.maximumChunkSize()));
+
         // Set LavaPlayer's MutableAudioFrame to use the same buffer as the one we just allocated
         frame.setBuffer(getBuffer());
-        this.player = player;
+
+        // Creates AudioPlayer instances and translates URLs to AudioTrack instances
+        this.playerManager = new DefaultAudioPlayerManager();
+
+        // This is an optimization strategy that Discord4J can utilize. It is not important to understand
+        this.playerManager.getConfiguration().setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
+
+        // Allow playerManager to parse remote sources like YouTube links
+        AudioSourceManagers.registerRemoteSources(this.playerManager);
+
+        // Create an AudioPlayer so Discord4J can receive audio data
+        this.player = playerManager.createPlayer();
+
+        this.scheduler = new TrackScheduler(this.player);
     }
 
     @Override
