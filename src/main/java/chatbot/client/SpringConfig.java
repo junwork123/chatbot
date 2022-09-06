2d3075b848565b17638029cd5ffd9c78230d9c0a
Junhyeok Choi <junwork123@gmail.com>
2022-09-06 20:22:20 +0900

    #refact : 사용하지 않는 bean을 config에서 제거

--- src/main/java/chatbot/client/SpringConfig.java
+++ src/main/java/chatbot/client/SpringConfig.java
@@ -1,65 +1,19 @@
 package chatbot.client;
 
-import chatbot.client.core.action.Action;
-import chatbot.client.core.action.ActionBuilder;
 import chatbot.client.core.ChatBot;
 import chatbot.client.core.ChatBotFactory;
-import chatbot.client.core.event.EventQueue;
-import chatbot.client.core.event.EventSensor;
-import chatbot.client.core.event.TaskRunner;
-import chatbot.client.domain.lostArkAuction.LostArkAuctionController;
-import chatbot.client.domain.lostArkAuction.LostArkAuctionServiceImpl;
-import chatbot.client.domain.pingpong.PingPongController;
-import chatbot.client.domain.pingpong.PingPongServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.List;
-
+@Slf4j
+@EnableScheduling
 @Configuration
 public class SpringConfig {
     @Bean
-    @Autowired
     public ChatBot chatBot(ChatBotFactory factory){
-        ActionBuilder actionBuilder = actionBuilder();
-        List<Action> actions = actionBuilder.getActions();
-        ChatBot chatBot = factory.CreateChatBot(actions);
+        ChatBot chatBot = factory.CreateChatBot();
         return chatBot;
-    }
-    @Bean
-    public ActionBuilder actionBuilder() {
-        return new ActionBuilder.Builder()
-                .addCommand("ping", new PingPongController(new PingPongServiceImpl()))
-                .addCommand("auction",new LostArkAuctionController(new LostArkAuctionServiceImpl()))
-//                .addCommand("play",new AudioController(new AudioService()))
-                .build();
-    }
-    @Bean
-    @ConditionalOnMissingBean
-    public EventQueue eventQueue(){
-        return new EventQueue();
-    }
-    public static final String EVENT_QUEUE_TREAD_POOL = "eventQueueTreadPool";
-    @Bean(name = EVENT_QUEUE_TREAD_POOL)
-    public ThreadPoolTaskExecutor eventQueueTreadPool() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(1000);
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        return executor;
-    }
-    @Bean
-    @ConditionalOnMissingBean
-    public TaskRunner taskRunner(
-            @Qualifier(EVENT_QUEUE_TREAD_POOL) ThreadPoolTaskExecutor executor,
-            EventQueue eventQueue,
-            List<EventSensor> eventSensors
-    ){
-        return new TaskRunner(executor, eventQueue, eventSensors);
     }
 }
