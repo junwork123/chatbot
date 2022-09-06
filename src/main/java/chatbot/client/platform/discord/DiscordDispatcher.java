d9237a9e0a8ddb0ba992923aabab76115516aa7b
Junhyeok Choi <junwork123@gmail.com>
2022-09-06 20:41:15 +0900

    #feat : dispatcher가 명령어를 인식하여 컨트롤러를 찾을 수 있도록 함

--- src/main/java/chatbot/client/platform/discord/DiscordDispatcher.java
+++ src/main/java/chatbot/client/platform/discord/DiscordDispatcher.java
@@ -1,18 +1,55 @@
 package chatbot.client.platform.discord;
 
-import chatbot.client.core.action.Action;
+import chatbot.client.core.command.Command;
 import chatbot.client.core.dispatcher.Dispatcher;
-import chatbot.client.core.message.MessageDto;
-import chatbot.client.utils.ApiUtils;
-import discord4j.core.event.domain.message.MessageCreateEvent;
+import chatbot.client.core.request.ChatRequest;
+import chatbot.client.core.request.MessageDto;
+import chatbot.client.core.result.ChatResult;
+import chatbot.client.core.result.DefaultChatResult;
+import chatbot.client.utils.BeanUtils;
+import chatbot.client.utils.ChatBotUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
-public class DiscordDispatcher implements Dispatcher<MessageCreateEvent> {
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import static chatbot.client.utils.ApiUtils.ApiResult;
+import static chatbot.client.utils.ApiUtils.success;
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class DiscordDispatcher implements Dispatcher {
+    private static final String MESSENGER = "DISCORD";
     @Override
-    public MessageDto dispatch(MessageCreateEvent event) {
-        return null;
+    public ApiResult<ChatRequest> dispatch(MessageDto dto) {
+        return success(
+                ChatRequest.builder()
+                        .messenger(MESSENGER)
+                        .command(dto.getCommand())
+                        .content(ChatBotUtils.parseCommand(dto.getContent(), dto.getCommand()))
+                        .build()
+                , ChatBotUtils.findController(dto.getCommand())
+        );
     }
-    @Override
-    public void onMessage(Action request, ApiUtils.ApiResult<MessageDto> result) {
 
+    @Override
+    public ApiResult<ChatResult> onMessage(ChatRequest request, Map<Class<?>, Method> map) {
+        Class<?> clazz = map.keySet().stream().findFirst().get();
+        Method method = map.get(clazz);
+        Object beanInstance = BeanUtils.getBean(clazz);
+        DefaultChatResult chatResult = null;
+        try {
+            chatResult = (DefaultChatResult) method.invoke(beanInstance, request.getContent());
+            log.info("invoke 직후 : " + chatResult.getMessage());
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+        return success(
+                chatResult
+                , null
+        );
     }
 }
