51f036d54a98ddd01016c745e6e4b112dabf8305
Junhyeok Choi <junwork123@gmail.com>
2022-09-06 20:41:00 +0900

    #refact : 명령어 인식 기능을 dispatcher로 이관

--- src/main/java/chatbot/client/core/ChatBotFactory.java
+++ src/main/java/chatbot/client/core/ChatBotFactory.java
@@ -1,9 +1,5 @@
 package chatbot.client.core;
 
-import chatbot.client.core.action.Action;
-
-import java.util.List;
-
 public interface ChatBotFactory {
-    public ChatBot CreateChatBot(List<Action> actions);
+    public ChatBot CreateChatBot();
 }
--- src/main/java/chatbot/client/platform/discord/DiscordChatBotFactory.java
+++ src/main/java/chatbot/client/platform/discord/DiscordChatBotFactory.java
@@ -1,6 +1,5 @@
 package chatbot.client.platform.discord;
 
-import chatbot.client.core.action.Action;
 import chatbot.client.core.ChatBot;
 import chatbot.client.core.ChatBotFactory;
 import chatbot.client.platform.discord.audio.LavaPlayerAudioProvider;
@@ -9,8 +8,6 @@ import discord4j.core.GatewayDiscordClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 /**
  * 로아봇 상태에 따른 이벤트 종류
@@ -27,16 +24,13 @@ import java.util.List;
 public class DiscordChatBotFactory implements ChatBotFactory {
     @Value("${DISCORD_TOKEN_ID}")
     private String DISCORD_TOKEN_ID;
-
     @Override
-    public ChatBot CreateChatBot(List<Action> actions) {
+    public ChatBot CreateChatBot() {
         // Discord 챗봇 생성
         GatewayDiscordClient client = DiscordClientBuilder.create(DISCORD_TOKEN_ID).build().login().block();
         LavaPlayerAudioProvider provider = new LavaPlayerAudioProvider();
-        DiscordChatBot chatBot = new DiscordChatBot(client, provider);
+        DiscordChatBot chatBot = new DiscordChatBot(client, provider, new DiscordDispatcher());
         chatBot.onCreated();
-        chatBot.registerActions(actions);
-        chatBot.registerMessageTemplates(List.of(DiscordMessageTemplate.values()));
         chatBot.onDestroy();
         return chatBot;
     }
